4815889e19aa6a837dda1ae48f8e167a2acba327
PopovMatvey <matyha550@gmail.com>
2024-08-19 16:56:23 +0300

    udp client validate send udp v 0.4.1

--- client/src/assets/components/PackageSenderUDP/index.tsx
+++ client/src/assets/components/PackageSenderUDP/index.tsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import "./css/style.css";
 import { request } from "../../hook/request";
+import { useNotificationWindowState } from "../ModalWindow/hook/notificationWindowState";
+import { ModalWindow } from "../ModalWindow";
 
 /**
  * 
  * @returns 
  */
 export function PackageSenderUDP() {
+    /* Hooks */
+    const {
+        titleText, bodyText,
+        modalWindowState, setModalWindowState,
+        getModalWindow
+    } = useNotificationWindowState();
     const [inputSendTextUDP, setInputSendTextUDP] = useState("");
 
 
@@ -33,7 +41,81 @@ export function PackageSenderUDP() {
     const handlerInputSendTextUDP = (event: any) => {
         setInputSendTextUDP(event.target.value);
 
+        console.log(validateString(event.target.value));
 
+        if (!validateString(event.target.value)) {
+            getModalWindow(`Внимание`, `Вы ввели неккоректные входные данные. ожидается набор значение HEX для пакета UDP`);
+        }
+
+
+
+        console.log(event.target.value.split(" "))
+    }
+
+    /**
+     * 
+     * @param parString 
+     * @returns 
+     */
+    const validateString = (parString: string) => {
+        for (let i = 0; i < parString.length; i++) {
+            if (!checkOnHex(parString[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 
+     * @param parChar 
+     * @returns 
+     */
+    const checkOnHex = (parChar: string) => {
+        switch (parChar) {
+            case '0': {
+                return true;
+            };
+            case '1': {
+                return true;
+            };
+            case '2': {
+                return true;
+            };
+            case '3': {
+                return true;
+            };
+            case '4': {
+                return true;
+            };
+            case '5': {
+                return true;
+            };
+            case '6': {
+                return true;
+            };
+            case '7': {
+                return true;
+            };
+            case '8': {
+                return true;
+            };
+            case '9': {
+                return true;
+            };
+            case '0': {
+                return true;
+            };
+            case 'x': {
+                return true;
+            };
+            case ' ': {
+                return true;
+            };
+        }
+
+        return false;
     }
 
     return (
@@ -52,6 +134,9 @@ export function PackageSenderUDP() {
 
                 </div>
             </div>
+            {
+                modalWindowState && <ModalWindow titleTextProp={titleText} bodyTextProp={bodyText} setModalWindowStateProp={setModalWindowState} />
+            }
         </>
     )
 }
